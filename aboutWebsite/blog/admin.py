61683ef1b7ac5fc6ed4eda05aed96825a9acb450
Krasi <krasi2405@gmail.com>
2017-07-06 23:26:45 +0300

    Created models and set them up in admin.

--- aboutWebsite/blog/admin.py
+++ aboutWebsite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, Image, Paragraph
+
+class ImageInLine(admin.StackedInline):
+	model = Image
+
+class ParagraphInLine(admin.StackedInline):
+	model = Paragraph
+
+class BlogAdmin(admin.ModelAdmin):
+	inlines = [ImageInLine, ParagraphInLine, ]
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Image)
+admin.site.register(Paragraph)
--- aboutWebsite/blog/models.py
+++ aboutWebsite/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length = 255)
+	created_at = models.DateTimeField(auto_now_add = True)
+	writer = models.CharField(max_length = 255)
+
+# Dont use just yet cant figure out how to upload images!
+class Image(models.Model):
+	src = models.ImageField()
+	blog = models.ForeignKey(Blog)
+	order = models.IntegerField(default = 0)
+
+
+class Paragraph(models.Model):
+	text = models.TextField()
+	blog = models.ForeignKey(Blog)
+	order = models.IntegerField(default = 0)
+
+
